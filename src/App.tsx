44f26363b2bafe0e108d650e03690698faa7c8a8
Karen Huang <56480921+kklhuang@users.noreply.github.com>
2026-05-26 22:13:55 -0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <img src="/landing_icon.png" alt="Image 1" className="image1" />
+        <img src="/landing_text.png" alt="Image 2" className="image2" />
+      </div>
+      <footer className="footer">
+        portfolio is currently under construction! come back again soon.
+      </footer>
+    </>
+  )
+}
+
+export default App
